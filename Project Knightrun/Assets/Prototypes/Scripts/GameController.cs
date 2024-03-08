acc8f3412b9a67a3a962421531ffa30f6d406e6f
Mahdi Said Ibrahim <46681526+Mahdidi00@users.noreply.github.com>
2024-03-08 20:04:29 +0100

    Expanded GameController
    Expanded GameController class with a few more functions

--- Project Knightrun/Assets/Prototypes/Scripts/GameController.cs
+++ Project Knightrun/Assets/Prototypes/Scripts/GameController.cs
@@ -4,33 +4,157 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    private void onGameStart()
-	{
-		Debug.Log("Game Start");
-	}
+    public List<Room> RoomPool = new List<Room>();
+    public List<Room> ActiveRooms = new List<Room>();
+    public Room CurrentRoom;
+    public int Score = 0;
+    public int Stage = 1;
+    public float GameSpeed = 30;
 
-	private void onGameOver()
-	{
-		Debug.Log("Game Over");
-	}
+    void Update()
+    {
+        
+    }
 
-	private void onPause()
-	{
-		Debug.Log("Game Paused");
-	}
+    public void onGameStart()
+    {
+        Debug.Log("Game Start");
+    }
 
-	private void onResume()
-	{
-		Debug.Log("Game Resume");
-	}
+    public void onGameOver()
+    {
+        for (int i = ActiveRooms.Count - 1; i >= 0; i--)
+        {
+            ActiveRooms[i].Speed = 0;
+        }
+        Debug.Log("Game Over");
+    }
 
-	private void SpawnRoom()
-	{
-		Debug.Log("Room Spawned");
-	}
+    public void onPause()
+    {
+        Debug.Log("Game Paused");
+    }
 
-	private void DespawnRoom()
-	{
-		Debug.Log("Room Despawned");
-	}
+    public void onResume()
+    {
+        for (int i = ActiveRooms.Count - 1; i >= 0; i--)
+        {
+            ActiveRooms[i].Speed = GameSpeed;
+        }
+        Debug.Log("Game Resume");
+    }
+
+    public void onStageChanged()
+    {
+        Stage++;
+        UpdateRoomPool();
+        Debug.Log("Stage Changed");
+    }
+
+    public void UpdateRoomPool()
+    {
+        Debug.Log("Room pool updated");
+    }
+
+    private GameObject SelectRoom()  //Numbers a, b, x, y, etc yet to decide
+    {
+      /*  switch (Stage) {
+
+            case 1:
+                if (Score < a)
+                {
+                    Debug.Log("Room Selected");
+                    return RoomPool[Random.Range(0, x)].gameObject;
+                }
+                else if (Score < b)
+                {
+                    return RoomPool[Random.Range(x, y)].gameObject;
+                }
+                else
+                {
+                    return RoomPool[Random.Range(y, RoomPool.Count)].gameObject;
+                }
+
+            case 2:
+                if (Score < c)
+                {
+                    Debug.Log("Room Selected");
+                    return RoomPool[Random.Range(0, x)].gameObject;
+                }
+                else if (Score < d)
+                {
+                    return RoomPool[Random.Range(x, y)].gameObject;
+                }
+                else
+                {
+                    return RoomPool[Random.Range(y, RoomPool.Count)].gameObject;
+                }
+
+            case 3:
+                if (Score < e)
+                {
+                    Debug.Log("Room Selected");
+                    return RoomPool[Random.Range(0, x)].gameObject;
+                }
+                else if (Score < f)
+                {
+                    return RoomPool[Random.Range(x, y)].gameObject;
+                }
+                else
+                {
+                    return RoomPool[Random.Range(y, RoomPool.Count)].gameObject;
+                }
+
+            case 4:
+                if (Score < g)
+                {
+                    Debug.Log("Room Selected");
+                    return RoomPool[Random.Range(0, x)].gameObject;
+                }
+                else if (Score < h)
+                {
+                    return RoomPool[Random.Range(x, y)].gameObject;
+                }
+                else
+                {
+                    return RoomPool[Random.Range(y, RoomPool.Count)].gameObject;
+                }
+
+            case 5:
+                if (Score < i)
+                {
+                    Debug.Log("Room Selected");
+                    return RoomPool[Random.Range(0, x)].gameObject;
+                }
+                else if (Score < j)
+                {
+                    return RoomPool[Random.Range(x, y)].gameObject;
+                }
+                else
+                {
+                    return RoomPool[Random.Range(y, RoomPool.Count)].gameObject;
+                }
+        }*/
+        Debug.Log("Room Selected");
+        return CurrentRoom.gameObject; //Placehoder
+    }
+
+
+    public void SpawnRoom()
+    {
+        GameObject newRoom = GameObject.Instantiate(SelectRoom(), new Vector3(), Quaternion.identity);
+        newRoom.GetComponent<Room>().Speed = GameSpeed;
+        ActiveRooms.Add(newRoom.GetComponent<Room>());
+        Debug.Log("Room Spawned");
+    }
+
+    public void DespawnRoom(GameObject _room)
+    {
+        for (int i = ActiveRooms.Count - 1; i >= 0; i--)
+        {
+            if (ActiveRooms[i].Equals(_room)) ActiveRooms.RemoveAt(i);
+        }
+        Destroy(_room);
+        Debug.Log("Room Despawned");
+    }
 }
