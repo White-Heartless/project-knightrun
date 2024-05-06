ffb1581e30013ec4a6cee444367d9881f7a6bbf9
Fabio <fabio.facilla@gmail.com>
2024-05-06 22:10:09 +0200

    fixed some last minor issues

--- Project Knightrun/Assets/Prototypes/Scripts/CameraSwitch.cs
+++ Project Knightrun/Assets/Prototypes/Scripts/CameraSwitch.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using com.cyborgAssets.inspectorButtonPro;
-using TreeEditor;
+//using TreeEditor;
 
 public class CameraSwitch : MonoBehaviour
 {
